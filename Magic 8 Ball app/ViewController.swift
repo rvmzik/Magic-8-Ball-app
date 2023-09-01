525e1bf0a3f54fe91885cda6203bb6736193c025
rvmzik <ramazan.aliyev.2001@bk.ru>
2023-09-01 09:47:49 +0600

    Initial Commit

---
+++ Magic 8 Ball app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball app
+//
+//  Created by Рамазан Алиев on 01.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
